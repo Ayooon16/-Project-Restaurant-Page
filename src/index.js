8449eb7c74d8e9b8e73bc8d81cda7c37b12da76b
ayooon <michaunapierala@gmail.com>
2024-03-23 21:34:53 +0100

    Allow to commit dist

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import './style.css';
-import { loadHome } from './modules/loadHome.js';
-import { loadAbout } from './modules/loadAbout.js';
-loadHome()
-window.onload=function(){
-    document.getElementById('home').onclick=loadHome;
-    document.getElementById('about').onclick=loadAbout;
-    };
