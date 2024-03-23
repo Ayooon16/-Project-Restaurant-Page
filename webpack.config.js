ca4b8a251296ba0bea0a04976f9b9ca61b9fceef
ayooon <michaunapierala@gmail.com>
2024-03-23 19:44:15 +0100

    Add css file

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,20 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, './dist'),
-    filename: 'index_bundle.js',
-  },
-  plugins: [new HtmlWebpackPlugin({
-    template: './src/index.html'
-  })],
+    entry: './src/index.js',
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+        ],
+    },
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: 'index_bundle.js',
+    },
+    plugins: [new HtmlWebpackPlugin({
+        template: './src/index.html'
+    })],
 };
